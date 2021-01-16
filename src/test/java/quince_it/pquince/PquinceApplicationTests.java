7f5659af82d43745dfd7bd79f4d4057e92f53019
dusancina98 <dule_srb@hotmail.rs>
2021-01-16 14:08:07 +0100

    Modify tests to run on pipeline

--- src/test/java/quince_it/pquince/PquinceApplicationTests.java
+++ src/test/java/quince_it/pquince/PquinceApplicationTests.java
@@ -1,13 +1,10 @@
 package quince_it.pquince;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import io.jsonwebtoken.lang.Assert;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class PquinceApplicationTests {
 
