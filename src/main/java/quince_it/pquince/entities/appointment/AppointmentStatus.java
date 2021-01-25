716f5d4d521d4c4a3f703500994360f37e0b8286
dusancina98 <dule_srb@hotmail.rs>
2021-01-25 16:08:01 +0100

    Add appointment entities

---
+++ src/main/java/quince_it/pquince/entities/appointment/AppointmentStatus.java
@@ -0,0 +1,9 @@
+package quince_it.pquince.entities.appointment;
+
+public enum AppointmentStatus {
+
+	CREATED,
+	CANCELED,
+	FINISHED,
+	EXPIRED
+}
---
+++ src/main/java/quince_it/pquince/entities/appointment/AppointmentType.java
@@ -0,0 +1,7 @@
+package quince_it.pquince.entities.appointment;
+
+public enum AppointmentType {
+
+	EXAMINATION,
+	CONSULTATION
+}
