9aa64992f7c32eb1936e72ee3a4dc7ad3d814bfb
jelena-petkovic <jelenaksipypetkovic@gmail.com>
2021-01-30 21:36:05 +0100

    Implement register pharmacy

--- PQuince.API/clientapp/src/pages/admin-registration/RegisterPharmacies.js
+++ PQuince.API/clientapp/src/pages/admin-registration/RegisterPharmacies.js
@@ -1,29 +1,26 @@
 import React, { Component } from "react";
 import Header from "../../components/Header";
 import TopBar from "../../components/TopBar";
-import CapsuleLogo from "../../static/capsuleLogo.png";
 import { BASE_URL } from "../../constants.js";
 import Axios from "axios";
+import ModalDialog from "../../components/ModalDialog";
+import { YMaps, Map } from "react-yandex-maps";
+
+const mapState = {
+	center: [44, 21],
+	zoom: 8,
+	controls: [],
+};
 
 class RegisterPharmacies extends Component {
 	state = {
-		drugs: [],
-	};
-
-	state = {
-		email: "",
-		password: "",
 		name: "",
-		surname: "",
+		description: "",
 		address: "",
-		phoneNumber: "",
-		emailError: "none",
-		passwordError: "none",
+		consulationPrice: "",
 		nameError: "none",
-		surnameError: "none",
 		addressError: "none",
-		phoneError: "none",
-		emailNotValid: "none",
+		consulationPriceError: "none",
 		openModal: false,
 		coords: [],
 	};
@@ -32,72 +29,102 @@ class RegisterPharmacies extends Component {
 		super(props);
 		this.addressInput = React.createRef();
 	}
-	
-	
-	handleEmailChange = (event) => {
-		this.setState({ email: event.target.value });
-	};
 
-	handlePasswordChange = (event) => {
-		this.setState({ password: event.target.value });
+	onYmapsLoad = (ymaps) => {
+		this.ymaps = ymaps;
+		new this.ymaps.SuggestView(this.addressInput.current, {
+			provider: {
+				suggest: (request, options) => this.ymaps.suggest(request),
+			},
+		});
 	};
 
 	handleNameChange = (event) => {
 		this.setState({ name: event.target.value });
 	};
 
-	handleSurnameChange = (event) => {
-		this.setState({ surname: event.target.value });
-	};
-
 	handleAddressChange = (event) => {
 		this.setState({ address: event.target.value });
 	};
 
-	handlePhoneNumberChange = (event) => {
-		this.setState({ phoneNumber: event.target.value });
+	handleConsulationPriceChange = (event) => {
+		this.setState({ consulationPrice: event.target.value });
+	};
+	
+	handleDescriptionChange = (event) => {
+		this.setState({ description: event.target.value });
 	};
 
 	validateForm = (userDTO) => {
 		this.setState({
-			emailError: "none",
-			emailNotValid: "none",
 			nameError: "none",
-			surnameError: "none",
 			addressError: "none",
-			phoneError: "none",
-			passwordError: "none",
+			consulationPriceError: "none",
 		});
 
-		if (userDTO.email === "") {
-			this.setState({ emailError: "initial" });
-			return false;
-		} else if (!userDTO.email.includes("@")) {
-			this.setState({ emailNotValid: "initial" });
-			return false;
-		} else if (userDTO.name === "") {
+		if (userDTO.name === "") {
 			this.setState({ nameError: "initial" });
-			return false;
-		} else if (userDTO.surname === "") {
-			this.setState({ surnameError: "initial" });
 			return false;
 		} else if (this.addressInput.current.value === "") {
 			this.setState({ addressError: "initial" });
 			return false;
-		} else if (userDTO.phoneNumber === "") {
-			this.setState({ phoneError: "initial" });
-			return false;
-		} else if (userDTO.password === "") {
-			this.setState({ passwordError: "initial" });
+		} else if (userDTO.consulationPrice === "") {
+			this.setState({ consulationPriceError: "initial" });
 			return false;
 		}
+		
 		return true;
 	};
 
 	handleModalClose = () => {
 		this.setState({ openModal: false });
 	};
+
+	handleSignUp = () => {
+		let street;
+		let city;
+		let country;
+		let latitude;
+		let longitude;
+
+		this.ymaps
+			.geocode(this.addressInput.current.value, {
+				results: 1,
+			})
+			.then(function (res) {
+				var firstGeoObject = res.geoObjects.get(0),
+					coords = firstGeoObject.geometry.getCoordinates();
+				latitude = coords[0];
+				longitude = coords[1];
+				country = firstGeoObject.getCountry();
+				street = firstGeoObject.getThoroughfare();
+				city = firstGeoObject.getLocalities().join(", ");
+			})
+			.then((res) => {
+				let pharmacyDTO = {
+					name: this.state.name,
+					description: this.state.description,
+					address: { street, country, city, latitude, longitude },
+					consultationPrice: this.state.consulationPrice*1,
+				};
+				console.log(pharmacyDTO);
+				if (this.validateForm(pharmacyDTO)) {
+				
+					Axios.post(BASE_URL + "/api/pharmacy", pharmacyDTO)
+						.then((res) => {
+							console.log("Success");
+							this.setState({ openModal: true });
+						})
+						.catch((err) => {
+							console.log(err);
+						});
+				}
+			});
+	};
 	
+	handleSelectChange  = (event) => {
+		this.setState({ selectValue: event.target.value });
+	};
 	
 	render() {
 		return (
@@ -107,7 +134,7 @@ class RegisterPharmacies extends Component {
 
 				<div className="container" style={{ marginTop: "8%" }}>
 					<h5 className=" text-center  mb-0 text-uppercase" style={{ marginTop: "2rem" }}>
-						Register pharmacy
+						Registrater pharmacy
 					</h5>
 
 					<div className="row section-design">
@@ -115,38 +142,7 @@ class RegisterPharmacies extends Component {
 							<br />
 							<form id="contactForm" name="sentMessage" novalidate="novalidate">
 								<div className="control-group">
-									<div
-										className="form-group controls mb-0 pb-2"
-										style={{ color: "#6c757d", opacity: 1 }}
-									>
-										<label>Email address:</label>
-										<input
-											placeholder="Email address"
-											className="form-control"
-											id="email"
-											type="text"
-											onChange={this.handleEmailChange}
-											value={this.state.email}
-										/>
-									</div>
-									<div
-										className="text-danger"
-										style={{ display: this.state.emailError }}
-									>
-										Email address must be entered.
-									</div>
-									<div
-										className="text-danger"
-										style={{ display: this.state.emailNotValid }}
-									>
-										Email address is not valid.
-									</div>
-								</div>
-								<div className="control-group">
-									<div
-										className="form-group controls mb-0 pb-2"
-										style={{ color: "#6c757d", opacity: 1 }}
-									>
+									<div className="form-group controls mb-0 pb-2" style={{ color: "#6c757d", opacity: 1 }}>
 										<label>Name:</label>
 										<input
 											placeholder="Name"
@@ -157,100 +153,67 @@ class RegisterPharmacies extends Component {
 											value={this.state.name}
 										/>
 									</div>
-									<div
-										className="text-danger"
-										style={{ display: this.state.nameError }}
-									>
+									<div className="text-danger" style={{ display: this.state.nameError }}>
 										Name must be entered.
 									</div>
 								</div>
+								
 								<div className="control-group">
-									<div
-										className="form-group controls mb-0 pb-2"
-										style={{ color: "#6c757d", opacity: 1 }}
-									>
-										<label>Surname:</label>
-										<input
-											placeholder="Surname"
-											class="form-control"
-											type="text"
-											id="surname"
-											onChange={this.handleSurnameChange}
-											value={this.state.surname}
-										/>
-									</div>
-									<div
-										className="text-danger"
-										style={{ display: this.state.surnameError }}
-									>
-										Surname must be entered.
-									</div>
-								</div>
-								<div className="control-group">
-									<div
-										className="form-group controls mb-0 pb-2"
-										style={{ color: "#6c757d", opacity: 1 }}
-									>
+									<div className="form-group controls mb-0 pb-2" style={{ color: "#6c757d", opacity: 1 }}>
 										<label>Address:</label>
-										<input
-											className="form-control"
-											id="suggest"
-											ref={this.addressInput}
-											placeholder="Address"
-										/>
+										<input className="form-control" id="suggest" ref={this.addressInput} placeholder="Address" />
 									</div>
-									
-									<div
-										className="text-danger"
-										style={{ display: this.state.addressError }}
+									<YMaps
+										query={{
+											load: "package.full",
+											apikey: "b0ea2fa3-aba0-4e44-a38e-4e890158ece2",
+											lang: "en_RU",
+										}}
 									>
+										<Map
+											style={{ display: "none" }}
+											state={mapState}
+											onLoad={this.onYmapsLoad}
+											instanceRef={(map) => (this.map = map)}
+											modules={["coordSystem.geo", "geocode", "util.bounds"]}
+										></Map>
+									</YMaps>
+									<div className="text-danger" style={{ display: this.state.addressError }}>
 										Address must be entered.
 									</div>
 								</div>
 								<div className="control-group">
-									<div
-										className="form-group controls mb-0 pb-2"
-										style={{ color: "#6c757d", opacity: 1 }}
-									>
-										<label>Phone number:</label>
+									<div className="form-group controls mb-0 pb-2" style={{ color: "#6c757d", opacity: 1 }}>
+										<label>Consultation price:</label>
 										<input
-											placeholder="Phone number"
+											placeholder="Consultation price"
 											class="form-control"
-											id="phone"
+											id="consulationPrice"
 											type="text"
-											onChange={this.handlePhoneNumberChange}
-											value={this.state.phoneNumber}
+											onChange={this.handleConsulationPriceChange}
+											value={this.state.consulationPrice}
 										/>
 									</div>
-									<div
-										className="text-danger"
-										style={{ display: this.state.phoneError }}
-									>
+									<div className="text-danger" style={{ display: this.state.consulationPriceError }}>
 										Phone number must be entered.
 									</div>
 								</div>
 								<div className="control-group">
-									<label>Password:</label>
-									<div
-										className="form-group controls mb-0 pb-2"
-										style={{ color: "#6c757d", opacity: 1 }}
-									>
+									<div className="form-group controls mb-0 pb-2" style={{ color: "#6c757d", opacity: 1 }}>
+										<label>Description:</label>
 										<input
-											placeholder="Password"
+											placeholder="Description"
 											class="form-control"
-											type="password"
-											onChange={this.handlePasswordChange}
-											value={this.state.password}
+											type="text"
+											id="name"
+											onChange={this.handleDescriptionChange}
+											value={this.state.description}
 										/>
 									</div>
-									<div
-										className="text-danger"
-										style={{ display: this.state.passwordError }}
-									>
-										Password must be entered.
+									<div className="text-danger" style={{ display: this.state.nameError }}>
+										Name must be entered.
 									</div>
 								</div>
-
 								<div className="form-group">
 									<button
 										style={{
@@ -271,7 +234,13 @@ class RegisterPharmacies extends Component {
 						</div>
 					</div>
 				</div>
-				
+				<ModalDialog
+					show={this.state.openModal}
+					href="/"
+					onCloseModal={this.handleModalClose}
+					header="Successful registration"
+					text="You have successfully registered staff."
+				/>
 			</React.Fragment>
 		);
 	}
