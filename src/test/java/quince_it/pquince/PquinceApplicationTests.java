fc7aa278d32a0c3748d66aae12300db9ec635360
dusancina98 <dule_srb@hotmail.rs>
2021-02-04 00:46:29 +0100

    Modify tests to run on junit5

--- src/test/java/quince_it/pquince/PquinceApplicationTests.java
+++ src/test/java/quince_it/pquince/PquinceApplicationTests.java
@@ -6,19 +6,18 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import quince_it.pquince.entities.drugs.DrugStorage;
 import quince_it.pquince.entities.users.Patient;
 import quince_it.pquince.repository.drugs.DrugStorageRepository;
 import quince_it.pquince.repository.users.PatientRepository;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class PquinceApplicationTests {
 	
@@ -28,134 +27,140 @@ public class PquinceApplicationTests {
 	@Autowired
 	private PatientRepository patientRepository;
 
-	@Test(expected = ObjectOptimisticLockingFailureException.class)
+	@Test
 	public void twoRequestForReduceAmountOfDrugInDrugStorageInARow() throws Throwable {
-		
-		ExecutorService executor = Executors.newFixedThreadPool(2);
-		Future<?> future1 = executor.submit(new Runnable() {
+		Assertions.assertThrows(ObjectOptimisticLockingFailureException.class, () -> {
+			ExecutorService executor = Executors.newFixedThreadPool(2);
+			Future<?> future1 = executor.submit(new Runnable() {
 
-			@Override
-			public void run() {
-		        System.out.println("Startovan Thread 1");
-		        
-		        DrugStorage drugStorage = drugStorageRepository.findByDrugIdAndPharmacyId(UUID.fromString("dac2b818-5838-11eb-ae93-0242ac130002"), UUID.fromString("cafeddee-56cb-11eb-ae93-0242ac130002"));
-				drugStorage.reduceAmount(20);
+				@Override
+				public void run() {
+			        System.out.println("Startovan Thread 1");
+			        
+			        DrugStorage drugStorage = drugStorageRepository.findByDrugIdAndPharmacyId(UUID.fromString("dac2b818-5838-11eb-ae93-0242ac130002"), UUID.fromString("cafeddee-56cb-11eb-ae93-0242ac130002"));
+					drugStorage.reduceAmount(20);
 
-				try { Thread.sleep(3000); } catch (InterruptedException e) {}
+					try { Thread.sleep(3000); } catch (InterruptedException e) {}
+					
+					drugStorageRepository.save(drugStorage);	
+
+				}
+			});
+			executor.submit(new Runnable() {
 				
-				drugStorageRepository.save(drugStorage);	
+				@Override
+				public void run() {
+			        System.out.println("Startovan Thread 2");
 
+					DrugStorage drugStorage = drugStorageRepository.findByDrugIdAndPharmacyId(UUID.fromString("dac2b818-5838-11eb-ae93-0242ac130002"), UUID.fromString("cafeddee-56cb-11eb-ae93-0242ac130002"));
+					drugStorage.reduceAmount(20);
+					drugStorageRepository.save(drugStorage);	
+
+				}
+			});
+			try {
+			    future1.get(); 
+			} catch (ExecutionException e) {
+			    System.out.println("Exception from thread " + e.getCause().getClass());
+			    throw e.getCause();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
+			executor.shutdown();
 		});
-		executor.submit(new Runnable() {
-			
-			@Override
-			public void run() {
-		        System.out.println("Startovan Thread 2");
-
-				DrugStorage drugStorage = drugStorageRepository.findByDrugIdAndPharmacyId(UUID.fromString("dac2b818-5838-11eb-ae93-0242ac130002"), UUID.fromString("cafeddee-56cb-11eb-ae93-0242ac130002"));
-				drugStorage.reduceAmount(20);
-				drugStorageRepository.save(drugStorage);	
-
-			}
-		});
-		try {
-		    future1.get(); 
-		} catch (ExecutionException e) {
-		    System.out.println("Exception from thread " + e.getCause().getClass());
-		    throw e.getCause();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		executor.shutdown();
+		
 	}
 	
-	@Test(expected = ObjectOptimisticLockingFailureException.class)
+	@Test
 	public void twoRequestForAddAmountOfDrugInDrugStorageInARow() throws Throwable {
-		
-		ExecutorService executor = Executors.newFixedThreadPool(2);
-		Future<?> future1 = executor.submit(new Runnable() {
+		Assertions.assertThrows(ObjectOptimisticLockingFailureException.class, () -> {
 
-			@Override
-			public void run() {
-		        System.out.println("Startovan Thread 1");
-
-		        DrugStorage drugStorage = drugStorageRepository.findByDrugIdAndPharmacyId(UUID.fromString("dac2b818-5838-11eb-ae93-0242ac130002"), UUID.fromString("cafeddee-56cb-11eb-ae93-0242ac130002"));
-				drugStorage.addAmount(5);
-
-				try { Thread.sleep(3000); } catch (InterruptedException e) {}
+			ExecutorService executor = Executors.newFixedThreadPool(2);
+			Future<?> future1 = executor.submit(new Runnable() {
+	
+				@Override
+				public void run() {
+			        System.out.println("Startovan Thread 1");
+	
+			        DrugStorage drugStorage = drugStorageRepository.findByDrugIdAndPharmacyId(UUID.fromString("dac2b818-5838-11eb-ae93-0242ac130002"), UUID.fromString("cafeddee-56cb-11eb-ae93-0242ac130002"));
+					drugStorage.addAmount(5);
+	
+					try { Thread.sleep(3000); } catch (InterruptedException e) {}
+					
+					drugStorageRepository.save(drugStorage);
+					
+					
+				}
+			});
+			executor.submit(new Runnable() {
 				
-				drugStorageRepository.save(drugStorage);
-				
-				
+				@Override
+				public void run() {
+			        System.out.println("Startovan Thread 2");
+	
+					DrugStorage drugStorage = drugStorageRepository.findByDrugIdAndPharmacyId(UUID.fromString("dac2b818-5838-11eb-ae93-0242ac130002"), UUID.fromString("cafeddee-56cb-11eb-ae93-0242ac130002"));
+					drugStorage.addAmount(5);					
+					drugStorageRepository.save(drugStorage);
+	
+				}
+			});
+			try {
+			    future1.get();
+			} catch (ExecutionException e) {
+			    System.out.println("Exception from thread " + e.getCause().getClass());
+			    throw e.getCause();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
+			executor.shutdown();
 		});
-		executor.submit(new Runnable() {
-			
-			@Override
-			public void run() {
-		        System.out.println("Startovan Thread 2");
-
-				DrugStorage drugStorage = drugStorageRepository.findByDrugIdAndPharmacyId(UUID.fromString("dac2b818-5838-11eb-ae93-0242ac130002"), UUID.fromString("cafeddee-56cb-11eb-ae93-0242ac130002"));
-				drugStorage.addAmount(5);					
-				drugStorageRepository.save(drugStorage);
-
-			}
-		});
-		try {
-		    future1.get();
-		} catch (ExecutionException e) {
-		    System.out.println("Exception from thread " + e.getCause().getClass());
-		    throw e.getCause();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		executor.shutdown();
 	}
 	
-	@Test(expected = ObjectOptimisticLockingFailureException.class)
+	@Test
 	public void concurentRequestForAddingPenaltyAndUpdatePatientsProfile() throws Throwable {
-		
-		ExecutorService executor = Executors.newFixedThreadPool(2);
-		Future<?> future1 = executor.submit(new Runnable() {
+		Assertions.assertThrows(ObjectOptimisticLockingFailureException.class, () -> {
 
-			@Override
-			public void run() {
-		        System.out.println("Startovan Thread 1");
-
-		        Patient patient = patientRepository.findById(UUID.fromString("22793162-52d3-11eb-ae93-0242ac130002")).get();
-				patient.addPenalty(1);
-
-				try { Thread.sleep(3000); } catch (InterruptedException e) {}
+			ExecutorService executor = Executors.newFixedThreadPool(2);
+			Future<?> future1 = executor.submit(new Runnable() {
+	
+				@Override
+				public void run() {
+			        System.out.println("Startovan Thread 1");
+	
+			        Patient patient = patientRepository.findById(UUID.fromString("22793162-52d3-11eb-ae93-0242ac130002")).get();
+					patient.addPenalty(1);
+	
+					try { Thread.sleep(3000); } catch (InterruptedException e) {}
+					
+					patientRepository.save(patient);
+					
+					
+				}
+			});
+			executor.submit(new Runnable() {
 				
-				patientRepository.save(patient);
-				
-				
+				@Override
+				public void run() {
+			        System.out.println("Startovan Thread 2");
+	
+			        Patient patient = patientRepository.findById(UUID.fromString("22793162-52d3-11eb-ae93-0242ac130002")).get();
+					patient.setName("Test");
+					patient.setPhoneNumber("Test");
+					patient.setSurname("Test");
+					patientRepository.save(patient);
+	
+				}
+			});
+			try {
+			    future1.get();
+			} catch (ExecutionException e) {
+			    System.out.println("Exception from thread " + e.getCause().getClass());
+			    throw e.getCause();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
+			executor.shutdown();
 		});
-		executor.submit(new Runnable() {
-			
-			@Override
-			public void run() {
-		        System.out.println("Startovan Thread 2");
-
-		        Patient patient = patientRepository.findById(UUID.fromString("22793162-52d3-11eb-ae93-0242ac130002")).get();
-				patient.setName("Test");
-				patient.setPhoneNumber("Test");
-				patient.setSurname("Test");
-				patientRepository.save(patient);
-
-			}
-		});
-		try {
-		    future1.get();
-		} catch (ExecutionException e) {
-		    System.out.println("Exception from thread " + e.getCause().getClass());
-		    throw e.getCause();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		executor.shutdown();
 	}
 
 }
