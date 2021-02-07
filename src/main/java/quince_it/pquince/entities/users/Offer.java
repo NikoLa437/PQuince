e8b96cc8f99bce6edb555cdbd58a21e505d79db8
jelena-petkovic <jelenaksipypetkovic@gmail.com>
2021-02-07 18:27:27 +0100

    Add offer logic and frontend

---
+++ src/main/java/quince_it/pquince/entities/users/Offer.java
@@ -0,0 +1,55 @@
+package quince_it.pquince.entities.users;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+public class Offer {
+
+	@Id
+    @Column(name = "id")
+	private UUID id;
+	
+	@Column(name = "price")
+	private double price;
+	
+	@Column(name = "dueToDate")
+	private Date dueToDate;
+	
+	public Offer(double price, Date dueToDate) {
+		this(UUID.randomUUID(), price,dueToDate);
+	}
+	public Offer(UUID id,double price, Date dueToDate) {
+		super();
+		this.price = price;
+		this.dueToDate = dueToDate;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Date getDueToDate() {
+		return dueToDate;
+	}
+
+	public void setDueToDate(Date dueToDate) {
+		this.dueToDate = dueToDate;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	
+	
+}
