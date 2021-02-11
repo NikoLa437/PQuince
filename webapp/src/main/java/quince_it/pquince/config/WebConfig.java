9c8ee37d4ba0e197996b3701025ce6cd6dda63f3
zindovicstefan <zindovicstefan98@gmail.com>
2021-02-11 18:38:05 +0100

    Implement change password on first login feature

--- webapp/src/main/java/quince_it/pquince/config/WebConfig.java
+++ webapp/src/main/java/quince_it/pquince/config/WebConfig.java
@@ -15,10 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
 	
 
 	
+	
 	/*
 	 * @Override public void addCorsMappings(CorsRegistry registry) {
 	 * registry.addMapping("/**").allowedOrigins("http://localhost:3000"); }
 	 */
+	 
 	 
 
 }
